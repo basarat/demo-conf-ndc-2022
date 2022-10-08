9f29852ce77549e01acf60088791cfbc827174d5
basarat <basaratali@gmail.com>
2022-10-08 11:34:18 +1100

    start

--- src/07 keyof/index.ts
+++ src/07 keyof/index.ts
@@ -3,13 +3,13 @@ type Person = {
   age: number,
 };
 
-function logGet(person: Person, key: keyof Person) {
+const jack: Person = { name: 'Jack', age: 20 };
+const jill: Person = { name: 'Jill', age: 30 };
+
+function logGet(person: Person, key: 'name' | 'age') {
   console.log('Getting', person, key);
   return person[key];
 }
-
-const jack: Person = { name: 'Jack', age: 20 };
-const jill: Person = { name: 'Jill', age: 30 };
 
 const jackAge = logGet(jack, 'age');
 const jillAge = logGet(jill, 'age');
