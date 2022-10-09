8ce59761b622f9fe0148f8acafa5bedbdb5ba457
Basarat <basaratali@gmail.com>
2022-10-10 09:07:19 +1100

    better

--- src/07 keyof/index.ts
+++ src/07 keyof/index.ts
@@ -7,6 +7,7 @@ const jack: Person = { name: 'Jack', age: 20 };
 const jill: Person = { name: 'Jill', age: 30 };
 
 // Can we make the key reliable?
+// Cane we make this generic?
 function logGet(person: Person, key: 'name' | 'age') {
   const value = person[key];
   console.log('Getting', person, key, value);
