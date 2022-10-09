a8a116f564c1aa84e3edaa1bc6fdf95455ad47c3
Basarat <basaratali@gmail.com>
2022-10-09 14:05:40 +1100

    better

---
+++ src/34 conditional never/index.ts
@@ -0,0 +1,11 @@
+function error(): never {
+  throw new Error('not implemented');
+}
+
+// const str: string = error();
+
+// const verbose: string | never = 'hello';
+
+// // null | never
+// type OnlyNull<T> = T extends null ? null : never;
+// type FilterNull = OnlyNull<string | null>;
