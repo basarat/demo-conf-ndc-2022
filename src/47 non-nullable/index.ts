f0b88485757c99d7054a75064a1c6b2fdc491141
basarat <basaratali@gmail.com>
2022-10-09 19:30:20 +1100

    better

--- src/47 non-nullable/index.ts
+++ src/47 non-nullable/index.ts
@@ -1,3 +1,12 @@
 export type StrOrNullOrUndefined = string | null | undefined;
 
 type Result = Exclude<StrOrNullOrUndefined, null | undefined>;
+
+type Union = { foo: number } | { bar: number };
+type UnionIntersection = Union & { bas: number };
+let example: UnionIntersection;
+example = { foo: 123, bas: 456 };
+example = { bar: 123, bas: 456 };
+
+type NullObject = null & {};
+type UndefinedObject = null & {};
